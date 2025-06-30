4dc758c84dd1ae84fa9a2f5a9f89d0bf98f7da93
Damian Jachowicz <damian.jachowicz@gmail.com>
2025-06-30 17:34:52 +0200

    chore: added cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,18 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), sitemap({ site: "https://7bb33e4c.snaick.pages.dev/" })],
+  integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
+    // @ts-expect-error - Type compatibility issue with Tailwind CSS v4 Vite plugin
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
   experimental: { session: true },
 });
